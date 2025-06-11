df11b6c85fc130e4472c16478217b67dc7a75d9d
geovana-clemente <geovana.c.cruz6@aluno.senai.br>
2025-06-11 13:59:39 -0300

    comit

--- while-play/projeto_whileplay/back-end/views/assinatura_list.php
+++ while-play/projeto_whileplay/back-end/views/assinatura_list.php
@@ -74,9 +74,9 @@
                 <td><?= htmlspecialchars($assinatura['cpf']) ?></td>
                 <td><?= htmlspecialchars($assinatura['data_assinatura']) ?></td>
                 <td>
-                    <a href="/while_play/update-assinatura/<?= $assinatura['id'] ?>">Atualizar</a>
+                    <a href="/while-play/projeto_whileplay/back-end/update-assinatura/<?= $assinatura['id'] ?>">Atualizar</a>
 
-                    <form action="/while_play/delete-assinatura" method="POST" style="display:inline;">
+                    <form action="/while-play/projeto_whileplay/back-end/delete-assinatura" method="POST" style="display:inline;">
                         <input type="hidden" name="nome" value="<?= $assinatura['nome'] ?>">
                         <button type="submit" onclick="return confirm('Tem certeza que deseja excluir esta assinatura?')">Excluir</button>
                     </form>
@@ -87,7 +87,7 @@
     </table>
 
     <br>
-   <a href="/while_play/public/assinatura">Nova assinatura</a>
+   <a href="/while-play/projeto_whileplay/back-end/public/assinatura">Nova assinatura</a>
 
 
 </body>
