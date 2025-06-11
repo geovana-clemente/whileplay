64386c905347bcc048834a170ffa6d32db653690
victor-do-vale <victor.v.souza7@aluno.senai.br>
2025-06-11 11:11:07 -0300

    comit

--- while-play/projeto_whileplay/back-end/public/index.php
+++ while-play/projeto_whileplay/back-end/public/index.php
@@ -9,9 +9,13 @@ error_reporting(E_ALL);
 require_once '../controllers/AssinaturaController.php';
 require_once '../controllers/RoteiroController.php';
 require_once '../controllers/PagamentoController.php';
+require_once '../controllers/PerfilController.php';
+require_once '../controllers/PersonagemController.php';
+require_once '../controllers/PublicarController.php';
+require_once '../controllers/SuporteController.php';
 
-// Capturar URL da requisição
-$request = $_SERVER['REQUEST_URI'];
+echo "REQUEST_URI: " . $_SERVER['REQUEST_URI'];
+exit;
 
 
 switch ($request) {
@@ -103,10 +107,132 @@ switch ($request) {
         $controller = new RoteiroController();
         $controller->updateRoteiro();
         break;
-    
+
+
+    case '/meu_projeto/public/perfil':
+        $controller = new PerfilController();
+        $controller->showForm();
+        break;
+
+    case '/meu_projeto/save-perfil':    
+        $controller = new PerfilController();
+        $controller->savePerfil();
+        break;
+
+    case '/meu_projeto/list-perfils':
+        $controller = new PerfilController();
+        $controller->listPerfils();
+        break;
+
+    case '/meu_projeto/delete-perfil':
+        $controller = new PerfilController();
+        $controller->deletePerfilById($_GET['id'] ?? null); // <-- cuidado com parâmetro
+        break;
+
+    case (preg_match('/\/meu_projeto\/update-perfil\/(\d+)/', $request, $matches) ? true : false):
+        $id = $matches[1];
+        $controller = new PerfilController();
+        $controller->showUpdateForm($id);
+        break;
+
+    case '/meu_projeto/update-perfil':
+        $controller = new PerfilController();
+        $controller->updatePerfil();
+        break;
+
+
+                    //pagamento
+    case '/meu_projeto/public/personagem':
+        $controller = new PersonagemController();
+        $controller->showForm();
+        break;
+    case '/meu_projeto/save-personagem':
+        $controller = new PersonagemController();
+        $controller->savePersonagem();
+        break;
+    case '/meu_projeto/list-personagens':
+        $controller = new PersonagemController();
+        $controller->listPersonagens();
+        break;
+    case '/meu_projeto/delete-personagem':
+        $controller = new PersonagemController();
+        $controller->deletePersonagemById();
+        break;
+    case (preg_match('/\/meu_projeto\/update-personagem\/(\d+)/', $request, $matches) ? true : false):
+        $id = $matches[1];
+        $controller = new PersonagemController();
+        $controller->showUpdateForm($id);
+        break;
+    case '/meu_projeto/update-personagem':
+        $controller = new PersonagemController();
+        $controller->updatePersonagem();
+        break;
+
+    // Publicar
+    case '/meu_projeto/public/publicar':
+        $controller = new PublicarController();
+        $controller->showForm();
+        break;
+        $controller = new PublicarController();
+        $controller->showForm();
+        break;
+    case '/meu_projeto/save-publicar':
+        $controller = new PublicarController();
+        $controller->savePublicar();
+        break;
+    case '/meu_projeto/list-publicados':
+        $controller = new PublicarController();
+        $controller->listPublicados();
+        break;
+    case '/meu_projeto/delete-publicar':
+        $controller = new PublicarController();
+        $controller->deletePublicarById($_POST['id'] ?? null);
+        break;
+    case (preg_match('/\/meu_projeto\/update-publicar\/(\d+)/', $request, $matches) ? true : false):
+        $id = $matches[1];
+        $controller = new PublicarController();
+        $controller->showUpdateForm($id);
+        break;
+    case '/meu_projeto/update-publicar':
+        $controller = new PublicarController();
+        $controller->updatePublicar();
+        break; 
+
+
+      //suporte
+         case '/meu_projeto/public/suporte':
+        $controller = new SuporteController();
+        $controller->showForm();
+        break;
+        $controller = new SuporteController();
+        $controller->showForm();
+        break;
+    case '/meu_projeto/save-suporte':
+        $controller = new SuporteController();
+        $controller->savePublicar();
+        break;
+    case '/meu_projeto/list-suportes':
+        $controller = new SuporteController();
+        $controller->listSuportes();
+        break;
+    case '/meu_projeto/delete-suporte':
+        $controller = new SuporteController();
+        $controller->deletePublicarById($_POST['id'] ?? null);
+        break;
+    case (preg_match('/\/meu_projeto\/update-suporte\/(\d+)/', $request, $matches) ? true : false):
+        $id = $matches[1];  
+        $controller = new SuporteController();
+        $controller->showUpdateForm($id);
+        break;
+    case '/meu_projeto/update-suporte':
+        $controller = new SuporteController();
+        $controller->updateSuporte();
+        break; 
+
+   
     default:
         http_response_code(404);
         echo $request;
         echo "Página não encontrada.";
         break;
-}
+} 
