2a0c58c15c42a22794c4833b8ff7b2319c3e7176
geovana-clemente <geovana.c.cruz6@aluno.senai.br>
2025-08-01 13:15:43 -0300

    oi

--- while-play/projeto_whileplay/back-end/models/Publicar.php
+++ while-play/projeto_whileplay/back-end/models/Publicar.php
@@ -1,6 +1,6 @@
 <?php
 
-class Personagem {
+class Publicar {
     private $pdo;
 
     public function __construct() {
@@ -8,36 +8,36 @@ class Personagem {
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    public function save($id_sobre, $mais_bem_avaliados, $lançados_recentemente, $caminho_imagem) {
+    public function save($usuario_id, $titulo, $sinopse, $tipo, $arquivo_url, $data_criacao, $publicado) {
         $stmt = $this->pdo->prepare("
-            INSERT INTO personagens (id_sobre, mais_bem_avaliados$, lançados_recentemente, caminho_imagem)
+            INSERT INTO publicados (usuario_id, titulo, sinopse, tipo, arquivo_url, data_criacao, publicado)
             VALUES (?, ?, ?, ?, ?)
         ");
-        $stmt->execute([$id_sobre, $mais_bem_avaliados, $lançados_recentemente, $caminho_imagem]);
+        $stmt->execute([$usuario_id, $titulo, $sinopse, $tipo, $arquivo_url, $data_criacao, $publicado]);
     }
 
     public function getAll() {
-        $stmt = $this->pdo->query("SELECT * FROM personagens");
+        $stmt = $this->pdo->query("SELECT * FROM publicados");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getById($id) {
-        $stmt = $this->pdo->prepare("SELECT * FROM personagens WHERE id = ?");
+        $stmt = $this->pdo->prepare("SELECT * FROM publicados WHERE id = ?");
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function update( $id_sobre, $mais_bem_avaliados, $lançados_recentemente, $caminho_imagem) {
+    public function update($id, $usuario_id, $titulo, $sinopse, $tipo, $arquivo_url, $data_criacao, $publicado) {
         $stmt = $this->pdo->prepare("
-            UPDATE personagens
-            SET id_sobre = ?, mais_bem_avaliados = ?, lançados_recentemente = ?, caminho_imagem = ?
+            UPDATE publicados
+            SET usuario_id = ?, titulo = ?, sinopse = ?, tipo = ?, arquivo_url, data_criacao, publicado = ?
             WHERE id = ?
         ");
-        $stmt->execute([$id_sobre, $mais_bem_avaliados, $lançados_recentemente, $caminho_imagem, $id]);
+        $stmt->execute([$usuario_id, $titulo, $sinopse, $tipo, $arquivo_url, $data_criacao, $publicado, $id]);
     }
 
     public function deleteById($id) {
-        $stmt = $this->pdo->prepare("DELETE FROM personagens WHERE id = ?");
+        $stmt = $this->pdo->prepare("DELETE FROM publicados WHERE id = ?");
         $stmt->execute([$id]);
     }
 }
