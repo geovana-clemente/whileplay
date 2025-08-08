31d7b278686cc05dbe0850018cfa17d82fb52076
geovana-clemente <geovana.c.cruz6@aluno.senai.br>
2025-08-08 07:25:48 -0300

    deletei paginas que não precisava

--- while-play/projeto_whileplay/back-end/models/db_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Helper to get PDO connection for models
-require_once __DIR__ . '/../config/database.php';
-
-function getDatabaseConnection() {
-    $database = new Database();
-    return $database->getConnection();
-}
--- while-play/projeto_whileplay/back-end/views/auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-// filepath: c:\Users\3anoA\Documents\WhilePlay\while-play\projeto_whileplay\back-end\views\auth_check.php
-// Inclua este arquivo no topo de páginas protegidas para exigir login
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header('Location: /while-play/projeto_whileplay/front-end/views/login.html');
-    exit;
-}
