3e4b3057e72b3f6754c490686c3467a06aa50add
victor-do-vale <victor.v.souza7@aluno.senai.br>
2025-11-05 14:27:56 -0300

    commit

--- while-play/projeto_whileplay/back-end/views/roteiro_form.php
+++ while-play/projeto_whileplay/back-end/views/roteiro_form.php
@@ -122,15 +122,6 @@
 
         <label for="visualizacoes">Visualizações:</label>
         <input type="number" id="visualizacoes" name="visualizacoes" value="0" min="0">
-            
-            <?php foreach ($assinaturas as $assinatura): ?>
-                <option value="<?= htmlspecialchars($assinatura['id']) ?>">
-                    Assinatura #<?= htmlspecialchars($assinatura['id']) ?> - 
-                    <?= htmlspecialchars($assinatura['nome_completo']) ?> - 
-                    <?= htmlspecialchars($assinatura['cidade']) ?> 
-                    (CEP: <?= htmlspecialchars($assinatura['cep']) ?>)
-                </option>
-            <?php endforeach; ?>
         </select>
 
         <input type="submit" value="Salvar Roteiro">
